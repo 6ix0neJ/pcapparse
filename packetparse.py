f902de75dd3ed961a5349382f303422d88aa1731
Jibril Richardson <jib.ish.rich@gmail.com>
2026-08-26 18:24:22 -0400

    Fixed directory searching

--- packetparse.py
+++ packetparse.py
@@ -2,6 +2,7 @@ from scapy.all import PcapReader
 from pathlib import Path
 import ipaddress
 import time
+import os
 
 def bannershow():
     print("████   ███   ███  ████     ████   ███  ████   ████ █████  ")
@@ -14,18 +15,20 @@ def bannershow():
     print(" ")
 
 bannershow()
+
 print("Packet Parser v1.0")
-pcapf = " "
+
+pcap_files = []
+
 localorspecific = input("Search for pcap file in current directory or specify path? (c/s): ")
-if localorspecific == "s":
-    pcapf = input("Pcap path: ")
-elif localorspecific  == "c":
+
+if localorspecific  == "c":
     print("Searching for pcap file recursively in the current directory...")
     directory = Path('.')
     # Find all .pcapng and .pcap files recursively
     pcap_files = list(directory.rglob('*.pcapng')) + list(directory.rglob('*.pcap'))
     print("Found ", len(pcap_files), " pcap files.")
-    print("Which should be used? (0 -", len(pcap_files)-1, ")")
+    print("Which should be used? (0 -", len(pcap_files)-1,")")
     for i, pcap_file in enumerate(pcap_files):
         print(f"{i}: {pcap_file}")
     selected_index = int(input("Enter the index of the pcap file to use: "))
@@ -34,12 +37,20 @@ elif localorspecific  == "c":
     else:
         print("Invalid index selected. Exiting.")
         exit()
+    pcapf = os.path.basename(pcapf)
+
+elif localorspecific == "s":
+    pcapf = input("Pcap path: ")
+    pcapf = os.path.basename(pcapf)
 
 if not pcap_files:
     print("No pcap files found.")
     exit()
 
 #pcapf = pcap_files[0]  # Use the first found pcap file
+print("Parsing", pcapf,"...")
+print("Analysis time: ",)
+print(type(pcapf))
 
 validips = []
 src_ips = []
@@ -91,10 +102,10 @@ with PcapReader(pcapf) as pcap_reader:
     stop_time = time.perf_counter()
 
 
-eleapsed_time = stop_time - start_time
-print("pcap analysis completed in ", eleapsed_time)
+elapsed_time = stop_time - start_time
+print("pcap analysis completed in", int(elapsed_time), "seconds.")
 
-print("Valid IPs found in the pcap file:", len(validips))
+print("Valid IPs found in the pcap file:", len(src_ips) + len(dst_ips))
 print("Protocols found:", len(protocols))
 showips = input("List valid Ips? (y/n): ")
 if showips == "y": iplist()
